3fffdc36abd81f3a827af730847cbfcd112b69a1
Carlos Matheu Armengol <info@plastikaweb.com>
2024-12-01 21:33:26 +0100

    chore: add terser for production build optimization

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,2 @@
 // Update copyright year
 document.getElementById('current-year').textContent = new Date().getFullYear();
-
-// Add smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     minify: 'terser',
     sourcemap: true,
     cssMinify: true,
     rollupOptions: {
+      input: {
+        main: './index.html',
+      },
       output: {
-        manualChunks: undefined,
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
       },
     },
   },
+  publicDir: 'src/assets',
 });
